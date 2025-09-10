f9e435e86b57b17be1f2ea8e82038bf763ba89ed
juanmatoniolo <juanmatoniolo2@gmail.com>
2025-09-10 02:54:42 -0300

    js

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,24 @@
+// pages/_app.js
+"use client"
+import { useEffect } from "react";
 
+function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      window.addEventListener("load", () => {
+        navigator.serviceWorker
+          .register("/progressier.js")
+          .then((registration) => {
+            console.log("ServiceWorker registrado con éxito:", registration);
+          })
+          .catch((error) => {
+            console.error("Error al registrar el ServiceWorker:", error);
+          });
+      });
+    }
+  }, []);
 
-export default function Home() {
-	return <>
-<h1>Zucc</h1>
-  </>
+  return <Component {...pageProps} />;
 }
+
+export default MyApp;
